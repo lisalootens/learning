fd7ff020f4f3699fd613082ae13cb5d727644098
lisalootens <lisa_lootens@live.nl>
2023-01-09 15:11:26 +0100

    Refactor code

--- Test Driven Development/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ Test Driven Development/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class ValidateISBNTest {
 
 	@Test
-	void checkAValidISBN() {
+	void checkAValid10ISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449116");
 		assertTrue(result, "first value");
@@ -15,37 +15,42 @@ class ValidateISBNTest {
 		assertTrue(result, "second value");
 	}
 	
+	@Test
+	void checkAValid13ISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("9781529105100");
+		assertTrue(result, "first value");
+		result = validator.checkISBN("9781853267338");
+		assertTrue(result, "second value");
+	}
+	
 	// Some valid ISBN numbers end with a X
 	@Test
-	void ISBNNumberEndingInXIsValid() {
+	void tenDigitISBNNumberEndingInXIsValid() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("012000030X");
 		assertTrue(result);
 	}
 	
 	@Test
-	void checkAnInvalidISBN() {
+	void checkAnInvalidTenDigitISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449117");
 		assertFalse(result);
 	}
 	
 	@Test
-	void checkForTenDigitISBN() {
+	void checkAnInvalid13DigitISBN() {
 		ValidateISBN validator = new ValidateISBN();
-		assertThrows(NumberFormatException.class, 
-				() -> {validator.checkISBN("123456789"); } );
+		boolean result = validator.checkISBN("9781529105101");
+		assertFalse(result);
 	}
 	
 	@Test
-	void checkFor13DigitISBN() {
+	void checkForDigitLength() {
 		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN("9781529105100");
-		
-		assertTrue(result);
-		
-		assertThrows(NumberFormatException.class,
-				() -> {validator.checkISBN("97815291051001");} );		
+		assertThrows(NumberFormatException.class, 
+				() -> {validator.checkISBN("123456789"); } );
 	}
 	
 	@Test
