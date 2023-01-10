264b69e7ca34e634625af92096c090f4591cad5d
lisalootens <lisa_lootens@live.nl>
2023-01-10 13:02:57 +0100

    Passing testService into stockManager

--- Test Driven Development/test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
+++ Test Driven Development/test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -5,19 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class StockManagementTests {
-	
-	ExternalISBNDataService testService = new ExternalISBNDataService() {
-		
-		@Override
-		public Book lookup(String isbn) {			
-			return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
-		}
-	};
 
 	@Test
 	void testCanGetACorrectLocatorCode() {
-		String isbn = "0140177396";
+		
+		ExternalISBNDataService testService = new ExternalISBNDataService() {
+			
+			@Override
+			public Book lookup(String isbn) {			
+				return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
+			}
+		};
+		
 		StockManager stockManager = new StockManager();
+		stockManager.setService(testService);
+		
+		String isbn = "0140177396";
 		String locatorCode = stockManager.getLocatorCode(isbn);
 		assertEquals("7396J4", locatorCode);
 	}
