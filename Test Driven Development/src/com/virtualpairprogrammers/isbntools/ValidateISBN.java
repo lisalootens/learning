d6dfcbeb86c5b9a722c915dcfb59a25dc1d64f7a
lisalootens <lisa_lootens@live.nl>
2023-01-10 11:41:40 +0100

    Add business logic for dealing with 13 digit long ISBN numbers

--- Test Driven Development/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ Test Driven Development/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -4,24 +4,44 @@ public class ValidateISBN {
 	
 	public boolean checkISBN(String isbn) {
 		
-		if (isbn.length() == 13) return true;
-		if (isbn.length() != 10) 
-			throw new NumberFormatException("ISBN number must be 10 or 13 digits long");
-		
-		int total = 0;
-		
-		for (int i = 0; i < 10; i++) {
-			if (!Character.isDigit(isbn.charAt(i)))  {
-				if(i == 9 && isbn.toUpperCase().charAt(i) == 'X') {
-					total += 10;
-				} 
+		if (isbn.length() == 13) {
+			int total = 0;
+			
+			for (int i = 0; i < 13; i++) {
+				if (i % 2 == 0) {
+					total += Character.getNumericValue(isbn.charAt(i));
+				}
 				else {
-					throw new NumberFormatException("ISBN number can only contain numeric digits");
+					total += Character.getNumericValue(isbn.charAt(i)) * 3;
 				}
 			}
 			
-			total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
-		} 
+			if (total % 10 == 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+					
+			if (isbn.length() != 10) 
+				throw new NumberFormatException("ISBN number must be 10 or 13 digits long");
+			
+			int total = 0;
+			
+			for (int i = 0; i < 10; i++) {
+				if (!Character.isDigit(isbn.charAt(i)))  {
+					if(i == 9 && isbn.toUpperCase().charAt(i) == 'X') {
+						total += 10;
+					} 
+					else {
+						throw new NumberFormatException("ISBN number can only contain numeric digits");
+					}
+				}
+				
+				total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
+			} 
 		
 		if (total % 11 == 0) {
 			return true;
@@ -30,4 +50,4 @@ public class ValidateISBN {
 			return false;
 		}
 	}
-}
+}}
